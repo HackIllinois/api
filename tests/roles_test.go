b28b6491435372060f05dbd16023eda59aab111c
Arnav Sankaran <arnavsankaran@gmail.com>
2018-05-07 18:59:40 -0500

    Added tests for role endpoints

---
+++ tests/roles_test.go
@@ -0,0 +1,69 @@
+package tests
+
+import (
+	"testing"
+	"reflect"
+	"net/http"
+	"net/http/httptest"
+	"encoding/json"
+	"github.com/hackillinois/api-auth/controller"
+	"github.com/hackillinois/api-auth/database"
+	"github.com/hackillinois/api-auth/models"
+	"github.com/hackillinois/api-auth/config"
+)
+
+func SetupTestDB(t *testing.T) {
+	err := database.Insert("roles", &models.UserRoles {
+		ID: "testid",
+		Roles: []string{"User"},
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func CleanupTestDB(t *testing.T) {
+	session := database.GetSession()
+	defer session.Close()
+
+	err := session.DB(config.AUTH_DB_NAME).DropDatabase()
+
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestGetRoles(t *testing.T) {
+	SetupTestDB(t)
+
+	req, err := http.NewRequest("GET", "/auth/roles/?id=testid", nil)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res_recorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(controller.GetRoles)
+
+	handler.ServeHTTP(res_recorder, req)
+
+	if res_recorder.Code != http.StatusOK {
+		t.Errorf("Wrong status code. Expected %v Got %v", http.StatusOK, res_recorder.Code)
+	}
+
+	var user_roles models.UserRoles
+	json.NewDecoder(res_recorder.Body).Decode(&user_roles)
+
+	if user_roles.ID != "testid" {
+		t.Errorf("Wrong user id. Expected %v, got %v", "testid", user_roles.ID)
+	}
+
+	expected_roles := []string{"User"}
+	if !reflect.DeepEqual(user_roles.Roles, expected_roles) {
+		t.Errorf("Wrong user id. Expected %v, got %v", expected_roles, user_roles.Roles)
+	}
+
+	CleanupTestDB(t)
+}

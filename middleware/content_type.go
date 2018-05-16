bbf1746c0ffbfd74223fd847af6a8eaa3486ba98
Arnav Sankaran <arnavsankaran@gmail.com>
2018-05-15 22:15:29 -0500

    Changed content type to match arbor's expected json value

--- middleware/content_type.go
+++ middleware/content_type.go
@@ -6,7 +6,7 @@ import (
 
 func ContentTypeMiddleware(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    	w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
         next.ServeHTTP(w, r)
     })
 }

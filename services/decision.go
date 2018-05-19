bd8a871ab410bb5a527a516750a8fe775928dc6f
Arnav Sankaran <arnavsankaran@gmail.com>
2018-05-18 21:54:25 -0500

    Added decision service endpoints

---
+++ services/decision.go
@@ -0,0 +1,46 @@
+package services
+
+import (
+	"github.com/HackIllinois/api-gateway/config"
+	"github.com/HackIllinois/api-gateway/middleware"
+	"github.com/arbor-dev/arbor"
+	"github.com/justinas/alice"
+	"net/http"
+)
+
+const DecisionURL = config.DecisionURL
+
+const DecisionFormat string = "JSON"
+
+var DecisionRoutes = arbor.RouteCollection{
+	arbor.Route{
+		"GetCurrentDecision",
+		"GET",
+		"/decision/",
+		alice.New(middleware.IdentificationMiddleware, middleware.AuthMiddleware([]string{"Attendee"})).ThenFunc(GetCurrentDecision).ServeHTTP,
+	},
+	arbor.Route{
+		"GetDecision",
+		"GET",
+		"/decision/{id}/",
+		alice.New(middleware.IdentificationMiddleware, middleware.AuthMiddleware([]string{"Admin"})).ThenFunc(GetDecision).ServeHTTP,
+	},
+	arbor.Route{
+		"UpdateDecision",
+		"POST",
+		"/decision/",
+		alice.New(middleware.IdentificationMiddleware, middleware.AuthMiddleware([]string{"Admin"})).ThenFunc(UpdateDecision).ServeHTTP,
+	},
+}
+
+func GetCurrentDecision(w http.ResponseWriter, r *http.Request) {
+	arbor.GET(w, DecisionURL+r.URL.String(), DecisionFormat, "", r)
+}
+
+func GetDecision(w http.ResponseWriter, r *http.Request) {
+	arbor.GET(w, DecisionURL+r.URL.String(), DecisionFormat, "", r)
+}
+
+func UpdateDecision(w http.ResponseWriter, r *http.Request) {
+	arbor.POST(w, DecisionURL+r.URL.String(), DecisionFormat, "", r)
+}

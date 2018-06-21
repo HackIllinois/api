f285c7c17fe9d220ccd15e52c6b7b56cbb67fe49
Pradyumna Shome <PradyGameDev+git@gmail.com>
2018-06-21 08:44:21 +0530

    Added a Finalized field to Decision and DecisionHistory structs

--- models/decision_history.go
+++ models/decision_history.go
@@ -1,6 +1,7 @@
 package models
 
 type DecisionHistory struct {
+	Finalized bool       `json:"finalized" validate:"required"`
 	ID        string     `json:"id"`
 	Status    string     `json:"status"`
 	Wave      int        `json:"wave"`

8f9c247f534db1cfa00347ea1b5ed06ebca705f2
Pradyumna Shome <PradyGameDev+git@gmail.com>
2018-06-24 22:38:39 +0530

    Sets FirstName and LastName in Create and Update registration functions

--- controller/controller.go
+++ controller/controller.go
@@ -72,6 +72,8 @@ func CreateCurrentUserRegistration(w http.ResponseWriter, r *http.Request) {
 
 	user_registration.GitHub = user_info.Username
 	user_registration.Email = user_info.Email
+	user_registration.FirstName = user_info.FirstName
+	user_registration.LastName = user_info.LastName
 
 	err = service.CreateUserRegistration(id, user_registration)
 

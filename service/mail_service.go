017985d4d43e56f502225aa99a326eb02c8d55b1
Pradyumna Shome <PradyGameDev+git@gmail.com>
2018-07-14 16:41:56 -0400

    Improvements to style and error-checking for user's addition to MailList

--- service/mail_service.go
+++ service/mail_service.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/HackIllinois/api-decision/config"
 	"github.com/HackIllinois/api-decision/models"
-	"gopkg.in/mgo.v2"
 )
 
 /*
@@ -30,37 +29,42 @@ func AddUserToMailList(id string, decision *models.DecisionHistory) error {
 		return errors.New("Decision status is not valid.")
 	}
 
-	mail_list := &models.MailList{
+	mail_list := models.MailList{
 		ID: mail_list_name,
 		UserIDs: []string{id},
 	}
 	
-	request_body, err := json.Marshal(mail_list)
+	request_body := bytes.Buffer{}
+	json.NewEncoder(&request_body).Encode(&mail_list)
 
-	if err != nil {
-		return err
-	}
-
-	request_body_buffer := bytes.NewBuffer(request_body)
-	
 	// URL to update the MailList with new IDs
 	api_mail_update_url := fmt.Sprintf("%s/list/add/", config.MAIL_SERVICE)
-	content_type := "application/json"
-	_, err_update := http.Post(api_mail_update_url, content_type, request_body_buffer)
 
-	if err_update == mgo.ErrNotFound {
-		// The mail list with given id does not exist. 
+	content_type := "application/json"
+
+	resp, err_update := http.Post(api_mail_update_url, content_type, &request_body)
+
+	if err_update == nil && resp.StatusCode != http.StatusOK {
+		// The mail list with given id does not exist.
 		// A new one will be created with the current user in it.
 		api_mail_create_url := fmt.Sprintf("%s/list/create/", config.MAIL_SERVICE)
-		_, err_create := http.Post(api_mail_create_url, content_type, request_body_buffer)
+		resp, err_create := http.Post(api_mail_create_url, content_type, &request_body)
 		
-		if err_create != nil {
+		if err_create == nil && resp.StatusCode != http.StatusOK {
+
+			return errors.New(fmt.Sprintf("Failed to create new MailList with id %s.", mail_list_name))	
+			
+		} else if err_create != nil {
+
+			// Error creating / executing the create POST request.
 			return err_create
 		}
-
-		return nil
 	} else if err_update != nil {
+
+		// Error creating / executing the update POST request.
 		return err_update
 	}
+
+	// The user should be in the correct mail list.
 	return nil
 }

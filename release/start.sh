0f665b0dc3043332355e8562f23d5cdd68b739f9
Arnav Sankaran <arnavsankaran@gmail.com>
2018-08-05 10:59:36 -0500

    Add release build resources

---
+++ release/start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Start gateway and api services
+./hackillinois-api-auth &
+./hackillinois-api-user &
+./hackillinois-api-registration &
+./hackillinois-api-decision &
+./hackillinois-api-rsvp &
+./hackillinois-api-checkin &
+./hackillinois-api-upload &
+./hackillinois-api-mail &
+./hackillinois-api-event &
+./hackillinois-api-stat &
+./hackillinois-api-gateway -u

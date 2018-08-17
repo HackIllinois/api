fdc111586b630af00b5620f5991272069ca60f7b
Arnav Sankaran <arnavsankaran@gmail.com>
2018-08-16 23:21:53 -0500

    Modify registration spec for rp

--- services/registration/models/user_registration.go
+++ services/registration/models/user_registration.go
@@ -1,46 +1,24 @@
 package models
 
 type UserRegistration struct {
-	ID                   string              `json:"id"                   validate:"required"`
-	FirstName            string              `json:"firstName"            validate:"required"`
-	LastName             string              `json:"lastName"             validate:"required"`
-	Email                string              `json:"email"                validate:"required,email"`
-	ShirtSize            string              `json:"shirtSize"            validate:"required,oneof=S M L XL"`
-	Diet                 string              `json:"diet"                 validate:"required,oneof=NONE VEGAN VEGETARIAN"`
-	Age                  int                 `json:"age"                  validate:"required"`
-	GraduationYear       int                 `json:"graduationYear"       validate:"required"`
-	Transportation       string              `json:"transportation"       validate:"required,oneof=NONE BUS"`
-	School               string              `json:"school"               validate:"required"`
-	Major                string              `json:"major"                validate:"required"`
-	Gender               string              `json:"gender"               validate:"required,oneof=MALE FEMALE NONBINARY OTHER"`
-	ProfessionalInterest string              `json:"professionalInterest" validate:"required,oneof=INTERNSHIP FULLTIME BOTH"`
-	GitHub               string              `json:"github"               validate:"required"`
-	Linkedin             string              `json:"linkedin"             validate:"required"`
-	Interests            string              `json:"interests"            validate:"required"`
-	IsNovice             bool                `json:"isNovice"             validate:"required|isdefault"`
-	IsPrivate            bool                `json:"isPrivate"            validate:"required|isdefault"`
-	PhoneNumber          string              `json:"phoneNumber"          validate:"required"`
-	LongForms            []UserLongForm      `json:"longforms"            validate:"required,dive,required"`
-	ExtraInfos           []UserExtraInfo     `json:"extraInfos"           validate:"required,dive,required"`
-	OsContributors       []UserOsContributor `json:"osContributors"       validate:"required,dive,required"`
-	Collaborators        []UserCollaborator  `json:"collaborators"        validate:"required,dive,required"`
-	CreatedAt            int64               `json:"createdAt"            validate:"required"`
-	UpdatedAt            int64               `json:"updatedAt"            validate:"required"`
-}
-
-type UserLongForm struct {
-	Response string `json:"response" validate:"required"`
-}
-
-type UserExtraInfo struct {
-	Response string `json:"response" validate:"required"`
-}
-
-type UserOsContributor struct {
-	Name        string `json:"name"        validate:"required"`
-	ContactInfo string `json:"contactInfo" validate:"required"`
-}
-
-type UserCollaborator struct {
-	Github string `json:"github" validate:"required"`
+	ID                   string   `json:"id"                   validate:"required"`
+	FirstName            string   `json:"firstName"            validate:"required"`
+	LastName             string   `json:"lastName"             validate:"required"`
+	Email                string   `json:"email"                validate:"required,email"`
+	GitHub               string   `json:"github"               validate:"required"`
+	PhoneNumber          string   `json:"phone"                validate:""`
+	Gender               string   `json:"gender"               validate:"required,oneof=MALE FEMALE NONBINARY NODISCLOSE OTHER"`
+	StudentType          string   `json:"studentType"          validate:"required,oneof=HIGHSCHOOL UNDERGRAD GRAD POSTGRAD ALUMNI NOSTUDENT NODISCLOSE"`
+	Major                string   `json:"major"                validate:"required"`
+	School               string   `json:"school"               validate:"required"`
+	Transportation       string   `json:"transportation"       validate:"required,oneof=ONCAMPUS BUS DRIVINGREIMBURSEMENT DRIVINGNOREIMBURSEMENT OTHER"`
+	ShirtSize            string   `json:"shirtSize"            validate:"required,oneof=S M L XL XXL"`
+	Diet                 string   `json:"diet"                 validate:"required,oneof=NONE VEGAN VEGETARIAN GLUTENFREE NOREDMEAT OTHER"`
+	GraduationClass      string   `json:"graduationClass"      validate:"required,oneof=FA18 SP19 FA19 SP20 FA20 SP21 FA21 SP22 FA22 SP23 AFTERSP23 NA"`
+	JobInterest          []string `json:"jobInterest"          validate:"required,dive,oneof=INTERNSHIP FULLTIME COOP CITYSCHOLAR NA"`
+	ProfessionalInterest []string `json:"professionalInterest" validate:"required,dive,oneof=AI SECURITY PARALLEL DATA HCI GFX ALGO THEORY HW NUMANALYSIS ML SWE CV QUANTUM CSO NETWORKING IS VIDEOGAME"`
+	HeardFrom            string   `json:"heardFrom"            validate:"required,dive,oneof=FB TWTR POSTERS EMAIL FRIEND ACM WEBSITE OTHER"`
+	RPInterest           []string `json:"rpInterest"           validate:"required,dive,oneof=SPEAKERS EXPERIENCES STARTUPFAIR CAREERFAIR MECHMANIA ACMSYMPOSIUM PUZZLEBANG ESCAPEROOM"`
+	CreatedAt            int64    `json:"createdAt"            validate:"required"`
+	UpdatedAt            int64    `json:"updatedAt"            validate:"required"`
 }

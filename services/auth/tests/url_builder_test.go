198a06db0d7ee38bebd46610a7b925ed2241450a
Patrick Gallagher <patrickj@cpgallagher.com>
2018-09-26 20:53:53 -0500

    Reformatted the url builder tests

--- services/auth/tests/url_builder_test.go
+++ services/auth/tests/url_builder_test.go
@@ -2,9 +2,9 @@ package tests
 
 import (
 	"github.com/HackIllinois/api/services/auth/service"
+	"net/url"
+	"strings"
 	"testing"
-    "strings"
-    "net/url"
 )
 
 const URL_SCHEME = "http"
@@ -17,17 +17,18 @@ const HOST_WITH_PORT = "hackillinois.com:9800"
 const CONSTRUCTED_COMPLEX_URL = "ftp://hackillinois.com:9800/register/now"
 
 var QUERY_PARAMS = map[string]string{
-    "major": "CS",
-    "graduating_year": "2021",
+	"major":           "CS",
+	"graduating_year": "2021",
 }
 
 const EXPECTED_QUERY_STRING = "?graduating_year=2021&major=CS"
 
 var QUERY_PARAMS_HASHTAG = map[string]string{
-    "major": "CS",
-    "twitter": "#hackillinois",
-    "dead_param": "true",
+	"major":      "CS",
+	"twitter":    "#hackillinois",
+	"dead_param": "true",
 }
+
 const EXPECTED_QUERY_STRING_HASHTAG = "?major=CS&twitter=#hackillinois&dead_param=true"
 
 /*
@@ -61,36 +62,35 @@ func TestConstructComplexURL(t *testing.T) {
 	}
 }
 
-
 /*
-    Test that building a simple query into a URL works.
+   Test that building a simple query into a URL works.
 */
 func TestQueryStringBuilder(t *testing.T) {
-    url := url.URL{
-        Scheme: URL_SCHEME,
-        Host: URL_HOST,
-        Path: URL_PATH,
-    }
+	url := url.URL{
+		Scheme: URL_SCHEME,
+		Host:   URL_HOST,
+		Path:   URL_PATH,
+	}
 
-    service.ConstructURLQuery(&url, QUERY_PARAMS)
+	service.ConstructURLQuery(&url, QUERY_PARAMS)
 
-    queryString := "?" + strings.Split(url.String(), "?")[1]
+	queryString := "?" + strings.Split(url.String(), "?")[1]
 
-    if queryString != EXPECTED_QUERY_STRING {
+	if queryString != EXPECTED_QUERY_STRING {
 		t.Errorf("Query string not correctly constructed. Expected \"%v\", got \"%v\"", EXPECTED_QUERY_STRING, queryString)
-    }
+	}
 }
 
 /*
-    Test that a URL with a scheme, host, path, and query params is correctly constructed.
+   Test that a URL with a scheme, host, path, and query params is correctly constructed.
 */
 func TestConstructFullURL(t *testing.T) {
 	result, err := service.ConstructSafeURL(URL_SCHEME, URL_HOST, URL_PATH, QUERY_PARAMS)
 
-    expectedURL := CONSTRUCTED_BASIC_URL + EXPECTED_QUERY_STRING
-    if result != expectedURL {
+	expectedURL := CONSTRUCTED_BASIC_URL + EXPECTED_QUERY_STRING
+	if result != expectedURL {
 		t.Errorf("URL not correctly constructed. Expected \"%v\", got \"%v\"", expectedURL, result)
-    }
+	}
 
 	if err != nil {
 		t.Fatal(err)
@@ -98,12 +98,12 @@ func TestConstructFullURL(t *testing.T) {
 }
 
 /*
-    Test that a URL with a fragment (`#` character) gets caught and throws an error.
+   Test that a URL with a fragment (`#` character) gets caught and throws an error.
 */
 func TestFragmentCaughtURL(t *testing.T) {
 	_, err := service.ConstructSafeURL(DIFFERENT_SCHEME, HOST_WITH_PORT, URL_PATH, QUERY_PARAMS_HASHTAG)
 
-    if err != service.HASHTAG_INVALID_ERR {
-        t.Error("The `#` in this URL was not caught!")
-    }
+	if err != service.HASHTAG_INVALID_ERR {
+		t.Error("The `#` in this URL was not caught!")
+	}
 }

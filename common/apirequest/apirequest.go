ef39b1514bb5ea55b3ed9f11bc0b6ea46a765f42
Arnav Sankaran <arnavsankaran@gmail.com>
2018-10-06 22:15:52 -0500

    Fix apirequest parameters

--- common/apirequest/apirequest.go
+++ common/apirequest/apirequest.go
@@ -16,7 +16,7 @@ var Identity = ""
 /*
 	Executes an API request and populates the data with the response
 */
-func Do(req *http.Request, data *interface{}) (int, error) {
+func Do(req *http.Request, data interface{}) (int, error) {
 	client := http.Client{
 		Timeout: time.Duration(Timeout) * time.Second,
 	}
@@ -39,35 +39,35 @@ func Do(req *http.Request, data *interface{}) (int, error) {
 /*
 	Executes an API GET request and populates the data with the response
 */
-func Get(url string, data *interface{}) (int, error) {
+func Get(url string, data interface{}) (int, error) {
 	return doRequest("GET", url, nil, data)
 }
 
 /*
 	Executes an API POST request and populates the data with the response
 */
-func Post(url string, payload *interface{}, data *interface{}) (int, error) {
+func Post(url string, payload interface{}, data interface{}) (int, error) {
 	return doRequest("POST", url, payload, data)
 }
 
 /*
 	Executes an API PUT request and populates the data with the response
 */
-func Put(url string, payload *interface{}, data *interface{}) (int, error) {
+func Put(url string, payload interface{}, data interface{}) (int, error) {
 	return doRequest("PUT", url, payload, data)
 }
 
 /*
 	Executes an API DELETE request and populates the data with the response
 */
-func Delete(url string, data *interface{}) (int, error) {
+func Delete(url string, data interface{}) (int, error) {
 	return doRequest("DELETE", url, nil, data)
 }
 
 /*
 	Builds a request, executes it, and then decodes the response into data
 */
-func doRequest(method string, url string, payload *interface{}, data *interface{}) (int, error) {
+func doRequest(method string, url string, payload interface{}, data interface{}) (int, error) {
 	var body *bytes.Buffer
 
 	if payload != nil {

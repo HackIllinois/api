eb1cd1d8094a689a5f2f3ca73be56e647a8a5d61
Arnav Sankaran <arnavsankaran@gmail.com>
2018-12-31 13:52:40 -0600

    Add health stat endpoints to services

--- common/apiserver/apiserver.go
+++ common/apiserver/apiserver.go
@@ -1,15 +1,24 @@
 package apiserver
 
 import (
+	"fmt"
 	"time"
 	"net/http"
+	"encoding/json"
 	"github.com/gorilla/mux"
+	"github.com/justinas/alice"
 	"github.com/HackIllinois/api/common/middleware"
+	"github.com/thoas/stats"
 )
 
 func StartServer(address string, router *mux.Router, name string) error {
 	router.Use(middleware.ErrorMiddleware)
 	router.Use(middleware.ContentTypeMiddleware)
+
+	stats_middleware := stats.New()
+	router.Use(stats_middleware.Handler)
+
+	router.Handle(fmt.Sprintf("/%s/internal/healthstats/", name), alice.New().ThenFunc(GetHealthStats(stats_middleware))).Methods("GET")
 
 	server := &http.Server{
 		Handler:      router,
@@ -19,4 +28,14 @@ func StartServer(address string, router *mux.Router, name string) error {
 	}
 
 	return server.ListenAndServe()
+}
+
+func GetHealthStats(stats_middleware *stats.Stats) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		health_stats := stats_middleware.Data()
+
+		w.Header().Set("Content-Type", "application/json")
+
+		json.NewEncoder(w).Encode(health_stats)
+	}
 }

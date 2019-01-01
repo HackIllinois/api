51714077c518da086214cc403b1ac5cf9b585d4d
Arnav Sankaran <arnavsankaran@gmail.com>
2019-01-01 16:56:40 -0600

    Check that give event name exists when favoriting

--- services/event/service/event_service.go
+++ services/event/service/event_service.go
@@ -366,6 +366,12 @@ func AddEventFavorite(id string, event string) error {
 		"id": id,
 	}
 
+	_, err := GetEvent(event)
+
+	if err != nil {
+		return errors.New("Could not find event with the given name")
+	}
+
 	event_favorites, err := GetEventFavorites(id)
 
 	if err != nil {

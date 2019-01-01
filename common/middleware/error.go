0c4e73b413842406abc61842ad6b31e56fb6b10f
Arnav Sankaran <arnavsankaran@gmail.com>
2019-01-01 15:40:13 -0600

    Add all errors at the service level

--- common/middleware/error.go
+++ common/middleware/error.go
@@ -7,17 +7,39 @@ import (
 	"net/http"
 )
 
+type ErrorLogEntry struct {
+	ID    string
+	Error interface{}
+}
+
+func LogError(id string, error_message interface{}) {
+	log_entry := ErrorLogEntry{
+		ID:    id,
+		Error: error_message,
+	}
+
+	error_log_message, err := json.Marshal(log_entry)
+
+	if err != nil {
+		fmt.Printf("Failed to marshal error for id: %v\n", id)
+		return
+	}
+
+	fmt.Printf("ERROR: %v\n", string(error_log_message))
+}
+
 func ErrorMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
-			if r := recover(); r != nil {
+			if rec := recover(); rec != nil {
 				w.Header().Set("Content-Type", "application/json")
-				switch panic_error := r.(type) {
+				switch panic_error := rec.(type) {
 				case errors.APIError:
+					LogError(r.Header.Get("HackIllinois-Identity"), panic_error)
 					w.WriteHeader(panic_error.Status)
 					json.NewEncoder(w).Encode(panic_error)
 				default:
-					fmt.Println(r)
+					LogError(r.Header.Get("HackIllinois-Identity"), rec)
 					w.WriteHeader(http.StatusInternalServerError)
 					json.NewEncoder(w).Encode(errors.APIError{Status: 500, Message: "Unknown Error"})
 				}

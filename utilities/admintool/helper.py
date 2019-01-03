eb1678f9e7e8474b9d47c689835ad6ea6c2bb96c
Arnav Sankaran <arnavsankaran@gmail.com>
2019-01-03 13:40:17 -0600

    Improve array formatting

--- utilities/admintool/helper.py
+++ utilities/admintool/helper.py
@@ -21,6 +21,8 @@ def dict_flatten(data, parent_key = '', seperator = '_'):
 		new_key = parent_key + seperator + k if parent_key else k
 		if isinstance(v, collections.MutableMapping):
 			items.extend(dict_flatten(v, new_key, seperator = seperator).items())
+		elif isinstance(v, list):
+			items.append((new_key, ','.join(v)))
 		else:
 			items.append((new_key, v))
 	return dict(items)

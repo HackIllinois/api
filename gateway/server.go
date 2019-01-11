620e7e64889ecd5d4ac5adfa729d065f757d3fe4
Arnav Sankaran <arnavsankaran@gmail.com>
2019-01-11 14:07:23 -0600

    Make gateway single binary compatible

--- gateway/server.go
+++ gateway/server.go
@@ -3,11 +3,11 @@ package gateway
 import (
 	"github.com/HackIllinois/api/gateway/config"
 	"github.com/HackIllinois/api/gateway/services"
-	"github.com/arbor-dev/arbor"
+	"github.com/arbor-dev/arbor/server"
 )
 
 func Entry() {
 	config.LoadArborConfig()
 	Routes := services.RegisterAPIs()
-	arbor.Boot(Routes, "0.0.0.0", config.GATEWAY_PORT)
+	server.StartUnsecuredServer(Routes.ToServiceRoutes(), "0.0.0.0", config.GATEWAY_PORT)
 }

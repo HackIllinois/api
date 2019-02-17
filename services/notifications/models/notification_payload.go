14ea6cd9b0fdcfbec0bd3b4201ac79acabe816e3
Ben Pankow <pankow.ben@gmail.com>
2019-02-17 00:55:25 -0600

    Add payload specifically for APNS sandbox

--- services/notifications/models/notification_payload.go
+++ services/notifications/models/notification_payload.go
@@ -1,7 +1,8 @@
 package models
 
 type NotificationPayload struct {
-	APNS    string `json:"APNS"`
-	GCM     string `json:"GCM"`
-	Default string `json:"default"`
+	APNS        string `json:"APNS"`
+	APNSSandbox string `json:"APNS_SANDBOX"`
+	GCM         string `json:"GCM"`
+	Default     string `json:"default"`
 }

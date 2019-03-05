f8c9073942728ee726a34048d5414285c6a75cf0
Arnav Sankaran <arnavsankaran@gmail.com>
2019-03-05 12:45:27 -0600

    Expose notification route through gateway

--- gateway/services/notifications.go
+++ gateway/services/notifications.go
@@ -16,26 +16,56 @@ var NotificationsRoutes = arbor.RouteCollection{
 	arbor.Route{
 		"GetAllTopics",
 		"GET",
-		"/notifications/",
+		"/notifications/topic/",
 		alice.New(middleware.IdentificationMiddleware, middleware.AuthMiddleware([]models.Role{models.AdminRole, models.StaffRole})).ThenFunc(GetAllTopics).ServeHTTP,
-	},
-	arbor.Route{
-		"GetAllNotifications",
-		"GET",
-		"/notifications/all/",
-		alice.New(middleware.IdentificationMiddleware, middleware.AuthMiddleware([]models.Role{models.UserRole})).ThenFunc(GetAllNotifications).ServeHTTP,
-	},
-	arbor.Route{
-		"GetPublicNotifications",
-		"GET",
-		"/notifications/public/",
-		alice.New(middleware.IdentificationMiddleware).ThenFunc(GetPublicNotifications).ServeHTTP,
 	},
 	arbor.Route{
 		"CreateTopic",
 		"POST",
-		"/notifications/",
+		"/notifications/topic/",
 		alice.New(middleware.IdentificationMiddleware, middleware.AuthMiddleware([]models.Role{models.AdminRole})).ThenFunc(CreateTopic).ServeHTTP,
+	},
+	arbor.Route{
+		"GetAllNotifications",
+		"GET",
+		"/notifications/topic/all/",
+		alice.New(middleware.IdentificationMiddleware, middleware.AuthMiddleware([]models.Role{models.UserRole})).ThenFunc(GetAllNotifications).ServeHTTP,
+	},
+	arbor.Route{
+		"GetAllPublicNotifications",
+		"GET",
+		"/notifications/topic/public/",
+		alice.New(middleware.IdentificationMiddleware).ThenFunc(GetAllPublicNotifications).ServeHTTP,
+	},
+	arbor.Route{
+		"GetNotificationsForTopic",
+		"GET",
+		"/notifications/topic/{id}/",
+		alice.New(middleware.IdentificationMiddleware, middleware.AuthMiddleware([]models.Role{models.AdminRole})).ThenFunc(GetNotificationsForTopic).ServeHTTP,
+	},
+	arbor.Route{
+		"PublishNotificationToTopic",
+		"POST",
+		"/notifications/topic/{id}/",
+		alice.New(middleware.IdentificationMiddleware, middleware.AuthMiddleware([]models.Role{models.AdminRole})).ThenFunc(PublishNotificationToTopic).ServeHTTP,
+	},
+	arbor.Route{
+		"DeleteTopic",
+		"DELETE",
+		"/notifications/topic/{id}/",
+		alice.New(middleware.IdentificationMiddleware, middleware.AuthMiddleware([]models.Role{models.AdminRole})).ThenFunc(DeleteTopic).ServeHTTP,
+	},
+	arbor.Route{
+		"SubscribeToTopic",
+		"POST",
+		"/notifications/topic/{id}/subscribe/",
+		alice.New(middleware.IdentificationMiddleware, middleware.AuthMiddleware([]models.Role{models.UserRole})).ThenFunc(SubscribeToTopic).ServeHTTP,
+	},
+	arbor.Route{
+		"UnsubscribeToTopic",
+		"POST",
+		"/notifications/topic/{id}/unsubscribe/",
+		alice.New(middleware.IdentificationMiddleware, middleware.AuthMiddleware([]models.Role{models.UserRole})).ThenFunc(UnsubscribeToTopic).ServeHTTP,
 	},
 	arbor.Route{
 		"RegisterDeviceToUser",
@@ -43,94 +73,44 @@ var NotificationsRoutes = arbor.RouteCollection{
 		"/notifications/device/",
 		alice.New(middleware.IdentificationMiddleware, middleware.AuthMiddleware([]models.Role{models.UserRole})).ThenFunc(RegisterDeviceToUser).ServeHTTP,
 	},
-	arbor.Route{
-		"UpdateUserSubscriptions",
-		"POST",
-		"/notifications/update/",
-		alice.New(middleware.IdentificationMiddleware, middleware.AuthMiddleware([]models.Role{models.UserRole})).ThenFunc(UpdateUserSubscriptions).ServeHTTP,
-	},
-	arbor.Route{
-		"GetNotificationsForTopic",
-		"GET",
-		"/notifications/{id}/",
-		alice.New(middleware.IdentificationMiddleware, middleware.AuthMiddleware([]models.Role{models.UserRole})).ThenFunc(GetNotificationsForTopic).ServeHTTP,
-	},
-	arbor.Route{
-		"DeleteTopic",
-		"DELETE",
-		"/notifications/{id}/",
-		alice.New(middleware.IdentificationMiddleware, middleware.AuthMiddleware([]models.Role{models.AdminRole})).ThenFunc(DeleteTopic).ServeHTTP,
-	},
-	arbor.Route{
-		"PublishNotification",
-		"POST",
-		"/notifications/{id}/",
-		alice.New(middleware.IdentificationMiddleware, middleware.AuthMiddleware([]models.Role{models.AdminRole})).ThenFunc(PublishNotification).ServeHTTP,
-	},
-	arbor.Route{
-		"AddUsersToTopic",
-		"POST",
-		"/notifications/{id}/add/",
-		alice.New(middleware.IdentificationMiddleware, middleware.AuthMiddleware([]models.Role{models.AdminRole, models.StaffRole})).ThenFunc(AddUsersToTopic).ServeHTTP,
-	},
-	arbor.Route{
-		"RemoveUsersFromTopic",
-		"POST",
-		"/notifications/{id}/remove/",
-		alice.New(middleware.IdentificationMiddleware, middleware.AuthMiddleware([]models.Role{models.AdminRole, models.StaffRole})).ThenFunc(RemoveUsersFromTopic).ServeHTTP,
-	},
-	arbor.Route{
-		"GetTopicInfo",
-		"GET",
-		"/notifications/{id}/info/",
-		alice.New(middleware.IdentificationMiddleware, middleware.AuthMiddleware([]models.Role{models.AdminRole})).ThenFunc(GetTopicInfo).ServeHTTP,
-	},
 }
 
 func GetAllTopics(w http.ResponseWriter, r *http.Request) {
 	arbor.GET(w, config.NOTIFICATIONS_SERVICE+r.URL.String(), NotificationsFormat, "", r)
 }
 
-func GetAllNotifications(w http.ResponseWriter, r *http.Request) {
-	arbor.GET(w, config.NOTIFICATIONS_SERVICE+r.URL.String(), NotificationsFormat, "", r)
-}
-
-func GetPublicNotifications(w http.ResponseWriter, r *http.Request) {
-	arbor.GET(w, config.NOTIFICATIONS_SERVICE+"/notifications/Attendee/", NotificationsFormat, "", r)
-}
-
 func CreateTopic(w http.ResponseWriter, r *http.Request) {
 	arbor.POST(w, config.NOTIFICATIONS_SERVICE+r.URL.String(), NotificationsFormat, "", r)
 }
 
-func GetTopicInfo(w http.ResponseWriter, r *http.Request) {
+func GetAllNotifications(w http.ResponseWriter, r *http.Request) {
 	arbor.GET(w, config.NOTIFICATIONS_SERVICE+r.URL.String(), NotificationsFormat, "", r)
 }
 
-func DeleteTopic(w http.ResponseWriter, r *http.Request) {
-	arbor.DELETE(w, config.NOTIFICATIONS_SERVICE+r.URL.String(), NotificationsFormat, "", r)
-}
-
-func PublishNotification(w http.ResponseWriter, r *http.Request) {
-	arbor.POST(w, config.NOTIFICATIONS_SERVICE+r.URL.String(), NotificationsFormat, "", r)
+func GetAllPublicNotifications(w http.ResponseWriter, r *http.Request) {
+	arbor.GET(w, config.NOTIFICATIONS_SERVICE+r.URL.String(), NotificationsFormat, "", r)
 }
 
 func GetNotificationsForTopic(w http.ResponseWriter, r *http.Request) {
 	arbor.GET(w, config.NOTIFICATIONS_SERVICE+r.URL.String(), NotificationsFormat, "", r)
 }
 
-func AddUsersToTopic(w http.ResponseWriter, r *http.Request) {
+func PublishNotificationToTopic(w http.ResponseWriter, r *http.Request) {
 	arbor.POST(w, config.NOTIFICATIONS_SERVICE+r.URL.String(), NotificationsFormat, "", r)
 }
 
-func RemoveUsersFromTopic(w http.ResponseWriter, r *http.Request) {
+func DeleteTopic(w http.ResponseWriter, r *http.Request) {
+	arbor.DELETE(w, config.NOTIFICATIONS_SERVICE+r.URL.String(), NotificationsFormat, "", r)
+}
+
+func SubscribeToTopic(w http.ResponseWriter, r *http.Request) {
+	arbor.POST(w, config.NOTIFICATIONS_SERVICE+r.URL.String(), NotificationsFormat, "", r)
+}
+
+func UnsubscribeToTopic(w http.ResponseWriter, r *http.Request) {
 	arbor.POST(w, config.NOTIFICATIONS_SERVICE+r.URL.String(), NotificationsFormat, "", r)
 }
 
 func RegisterDeviceToUser(w http.ResponseWriter, r *http.Request) {
-	arbor.POST(w, config.NOTIFICATIONS_SERVICE+r.URL.String(), NotificationsFormat, "", r)
-}
-
-func UpdateUserSubscriptions(w http.ResponseWriter, r *http.Request) {
 	arbor.POST(w, config.NOTIFICATIONS_SERVICE+r.URL.String(), NotificationsFormat, "", r)
 }

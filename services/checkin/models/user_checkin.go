47c252140dc47e3560e9a285c0fea370edb42db0
Arnav Sankaran <arnavsankaran@gmail.com>
2019-03-19 22:04:11 -0500

    Copy through rsvp data

--- services/checkin/models/user_checkin.go
+++ services/checkin/models/user_checkin.go
@@ -1,8 +1,9 @@
 package models
 
 type UserCheckin struct {
-	ID              string `json:"id"`
-	Override        bool   `json:"override"`
-	HasCheckedIn    bool   `json:"hasCheckedIn"`
-	HasPickedUpSwag bool   `json:"hasPickedUpSwag"`
+	ID              string                 `json:"id"`
+	Override        bool                   `json:"override"`
+	HasCheckedIn    bool                   `json:"hasCheckedIn"`
+	HasPickedUpSwag bool                   `json:"hasPickedUpSwag"`
+	RsvpData        map[string]interface{} `json:"rsvpData"`
 }

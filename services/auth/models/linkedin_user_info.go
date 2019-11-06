3b86a8b3e5277595e92d1adb4612a3778580d6d6
Bing-Yuan Hsieh <hsiehby@gmail.com>
2019-11-06 02:36:57 -0600

    Worked on LinkedIn API v2 GetUserInfo() migration. Still need testing.

--- services/auth/models/linkedin_user_info.go
+++ services/auth/models/linkedin_user_info.go
@@ -1,9 +1,30 @@
 package models
 
+// ISSUE: formattedName no longer available in API v2
 type LinkedinUserInfo struct {
 	ID        string `json:"id"`
-	Username  string `json:"formattedName"`
 	Email     string `json:"emailAddress"`
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
+	FirstName struct {
+		Localized       map[string]string `json:"localized"`
+		PreferredLocale struct {
+			Country  string `json:"country"`
+			Language string `json:"language"`
+		} `json:"preferredLocale"`
+	} `json:"firstName"`
+	LastName struct {
+		Localized       map[string]string `json:"localized"`
+		PreferredLocale struct {
+			Country  string `json:"country"`
+			Language string `json:"language"`
+		} `json:"preferredLocale"`
+	} `json:"lastName"`
 }
+
+// Response format for FirstName and LastName is in MultiLocaleString
+// "localized":{
+// 	"en_US":"2029 Stierlin Ct, Mountain View, CA 94043"
+// },
+// "preferredLocale":{
+// 	"country":"US",
+// 	"language":"en"
+// }
--- services/auth/service/linkedin_service.go
+++ services/auth/service/linkedin_service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"errors"
+
 	"github.com/HackIllinois/api/services/auth/config"
 	"github.com/HackIllinois/api/services/auth/models"
 	"github.com/levigross/grequests"
@@ -26,7 +27,7 @@ func (provider *LinkedInOAuthProvider) GetAuthorizationRedirect(redirect_uri str
 	return ConstructSafeURL("https", "www.linkedin.com", "oauth/v2/authorization",
 		map[string]string{
 			"client_id":     config.LINKEDIN_CLIENT_ID,
-			"scope":         "r_basicprofile r_emailaddress",
+			"scope":         "r_liteprofile r_emailaddress", // changed from r_basicprofile to r_liteprofile
 			"response_type": "code",
 			"redirect_uri":  redirect_uri,
 		},
@@ -78,7 +79,16 @@ func (provider *LinkedInOAuthProvider) Authorize(code string, redirect_uri strin
 func (provider *LinkedInOAuthProvider) GetUserInfo() (*models.UserInfo, error) {
 	var user_info models.UserInfo
 
-	request, err := grequests.Get("https://api.linkedin.com/v1/people/~:(id,formatted-name,email-address,first-name,last-name)", &grequests.RequestOptions{
+	// request, err := grequests.Get("https://api.linkedin.com/v1/people/~:(id,formatted-name,email-address,first-name,last-name)", &grequests.RequestOptions{
+	// 	Headers: map[string]string{
+	// 		"Authorization": "Bearer " + provider.token,
+	// 		"Content-Type":  "application/json",
+	// 		"x-li-format":   "json",
+	// 	},
+	// })
+
+	// ISSUE: The formatted-name field is no longer available
+	request, err := grequests.Get("https://api.linkedin.com/v2/me?projection=(id,firstName?fields=id,bar,baz,lastName)", &grequests.RequestOptions{
 		Headers: map[string]string{
 			"Authorization": "Bearer " + provider.token,
 			"Content-Type":  "application/json",
@@ -102,10 +112,49 @@ func (provider *LinkedInOAuthProvider) GetUserInfo() (*models.UserInfo, error) {
 	}
 
 	user_info.ID = "linkedin" + linkedin_user_info.ID
-	user_info.Username = linkedin_user_info.Username
-	user_info.Email = linkedin_user_info.Email
-	user_info.FirstName = linkedin_user_info.FirstName
-	user_info.LastName = linkedin_user_info.LastName
+
+	preferred_country := linkedin_user_info.FirstName.PreferredLocale.Country
+	preferred_language := linkedin_user_info.FirstName.PreferredLocale.Language
+	if preferred_country != "" && preferred_language != "" {
+		// preferred local is provided
+		preferred_locale := preferred_language + "_" + preferred_country
+		user_info.FirstName = linkedin_user_info.FirstName.Localized[preferred_locale]
+		user_info.LastName = linkedin_user_info.LastName.Localized[preferred_locale]
+		user_info.Username = linkedin_user_info.FirstName.Localized[preferred_locale] + " " + linkedin_user_info.LastName.Localized[preferred_locale]
+	} else {
+		// preferred local is not provided, try getting en_US first, if failed, pick an arbitrary one
+		if linkedin_user_info.FirstName.Localized["en_US"] != "" && linkedin_user_info.LastName.Localized["en_US"] != "" {
+			user_info.FirstName = linkedin_user_info.FirstName.Localized["en_US"]
+			user_info.LastName = linkedin_user_info.LastName.Localized["en_US"]
+			user_info.Username = linkedin_user_info.FirstName.Localized["en_US"] + "_" + linkedin_user_info.LastName.Localized["en_US"]
+		} else {
+			for locale, _ := range linkedin_user_info.FirstName.Localized {
+				arbitrary_locale := locale
+				user_info.FirstName = linkedin_user_info.FirstName.Localized[arbitrary_locale]
+				user_info.LastName = linkedin_user_info.LastName.Localized[arbitrary_locale]
+				user_info.Username = linkedin_user_info.FirstName.Localized[arbitrary_locale] + "_" + linkedin_user_info.LastName.Localized[arbitrary_locale]
+				break
+			}
+		}
+	}
+
+	// In API v2, a seperate request is required to retrieve the email address
+	request, err = grequests.Get("https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))", &grequests.RequestOptions{
+		Headers: map[string]string{
+			"Authorization": "Bearer " + provider.token,
+			"Content-Type":  "application/json",
+			"x-li-format":   "json",
+		},
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	var email models.LinkedinEmail
+	err = request.JSON(&email)
+
+	user_info.Email = email.Email
 
 	provider.isVerifiedUser = true
 

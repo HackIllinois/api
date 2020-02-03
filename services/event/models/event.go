845ba932027739fc06977a1529355476f73419f1
Ben Pankow <pankow.ben@gmail.com>
2020-02-03 14:12:41 -0600

    Add tags field to event locations (#305)
    * Add location tags to events
    
    * Update docs
    
    * Remove tag validation

--- services/event/models/event.go
+++ services/event/models/event.go
@@ -12,7 +12,8 @@ type Event struct {
 }
 
 type EventLocation struct {
-	Description string  `json:"description" validate:"required"`
-	Latitude    float64 `json:"latitude"    validate:"required"`
-	Longitude   float64 `json:"longitude"   validate:"required"`
+	Description string   `json:"description" validate:"required"`
+	Tags        []string `json:"tags"        validate:"required"`
+	Latitude    float64  `json:"latitude"    validate:"required"`
+	Longitude   float64  `json:"longitude"   validate:"required"`
 }

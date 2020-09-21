e67062e78c7a483aedb49af4c30261e1ce9c490c
Varun-S-Nair <varunsnair2002@gmail.com>
2020-09-21 15:03:41 -0500

    Removed hasPickedUpSwag

--- services/checkin/models/user_checkin.go
+++ services/checkin/models/user_checkin.go
@@ -4,6 +4,5 @@ type UserCheckin struct {
 	ID              string                 `json:"id"`
 	Override        bool                   `json:"override"`
 	HasCheckedIn    bool                   `json:"hasCheckedIn"`
-	HasPickedUpSwag bool                   `json:"hasPickedUpSwag"`
 	RsvpData        map[string]interface{} `json:"rsvpData"`
 }

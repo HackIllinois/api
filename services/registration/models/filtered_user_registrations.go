4a3c01db0f87b88b3fc03972f6a6dc451977c871
TakshPSingh <taksh2@illinois.edu>
2021-11-03 18:04:12 -0500

    Return empty array instead of null for registration endpoints

--- services/registration/models/filtered_user_registrations.go
+++ services/registration/models/filtered_user_registrations.go
@@ -1,5 +1,24 @@
 package models
 
+import (
+	"encoding/json"
+)
+
 type FilteredUserRegistrations struct {
 	Registrations []UserRegistration `json:"registrations"`
+}
+
+func (original FilteredUserRegistrations) MarshalJSON() ([]byte, error) {
+	type Alias FilteredUserRegistrations
+
+	modified := struct {
+		Alias
+	}{
+		Alias: (Alias)(original),
+	}
+
+	if modified.Registrations == nil {
+		modified.Registrations = make([]UserRegistration, 0)
+	}
+	return json.Marshal(modified)
 }

a37c656518cc2611e23195c5554b72fe292292b9
Jareth Gomes <jarethgomes@gmail.com>
2022-08-10 17:52:34 -0500

    Added e2e tests for GET /event/favorite/

--- tests/e2e/events_test/events_test.go
+++ tests/e2e/events_test/events_test.go
@@ -1064,6 +1064,63 @@ func TestAddFavoriteEventNotExist(t *testing.T) {
 	}
 }
 
+func TestGetFavoriteEventsNormal(t *testing.T) {
+	CreateEvents()
+	defer ClearEvents()
+
+	favorite_events := event_models.EventFavorites{
+		ID: "localadmin",
+		Events: []string{
+			"event1",
+			"event2",
+		},
+	}
+
+	client.Database(events_db_name).Collection("favorites").InsertOne(context.Background(), favorite_events)
+
+	recieved_favorites := event_models.EventFavorites{}
+	response, err := user_client.New().Get("/event/favorite/").ReceiveSuccess(&recieved_favorites)
+
+	if err != nil {
+		t.Fatal("Unable to make request")
+		return
+	}
+	if response.StatusCode != http.StatusOK {
+		t.Fatalf("Request returned HTTP error %d", response.StatusCode)
+		return
+	}
+
+	if !reflect.DeepEqual(recieved_favorites, favorite_events) {
+		t.Fatalf("Wrong result received from database. Expected %v, got %v", favorite_events, recieved_favorites)
+	}
+}
+
+func TestGetFavoriteEventsNone(t *testing.T) {
+	CreateEvents()
+	defer ClearEvents()
+
+	favorite_events := event_models.EventFavorites{
+		ID:     "localadmin",
+		Events: []string{},
+	}
+
+	recieved_favorites := event_models.EventFavorites{}
+	response, err := user_client.New().Get("/event/favorite/").ReceiveSuccess(&recieved_favorites)
+
+	if err != nil {
+		t.Fatal("Unable to make request")
+		return
+	}
+	if response.StatusCode != http.StatusOK {
+		t.Fatalf("Request returned HTTP error %d", response.StatusCode)
+		return
+	}
+
+	if !reflect.DeepEqual(recieved_favorites, favorite_events) {
+		t.Fatalf("Wrong result received from database. Expected %v, got %v", favorite_events, recieved_favorites)
+	}
+}
+
 func TestStaffActions(t *testing.T) {
 	// 1. Create event
 	event_info := event_models.Event{

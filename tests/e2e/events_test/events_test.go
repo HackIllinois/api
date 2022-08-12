ec40830b1f4fda2279f6d3eb3849aa3060099ac2
Jareth Gomes <jarethgomes@gmail.com>
2022-08-12 16:32:54 -0500

    Added e2e tests for POST /event/track/

--- tests/e2e/events_test/events_test.go
+++ tests/e2e/events_test/events_test.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/HackIllinois/api/common/configloader"
+	checkin_models "github.com/HackIllinois/api/services/checkin/models"
 	event_models "github.com/HackIllinois/api/services/event/models"
 	profile_models "github.com/HackIllinois/api/services/profile/models"
 	"github.com/HackIllinois/api/tests/common"
@@ -27,6 +28,7 @@ var client *mongo.Client
 
 var events_db_name string
 var profile_db_name string
+var checkin_db_name string
 
 var current_unix_time = time.Now().Unix()
 
@@ -55,6 +57,11 @@ func TestMain(m *testing.M) {
 		fmt.Printf("ERROR: %v\n", err)
 		os.Exit(1)
 	}
+	checkin_db_name, err = cfg.Get("CHECKIN_DB_NAME")
+	if err != nil {
+		fmt.Printf("ERROR: %v\n", err)
+		os.Exit(1)
+	}
 	ResetDatabase()
 
 	return_code := m.Run()
@@ -65,6 +72,7 @@ func TestMain(m *testing.M) {
 func ResetDatabase() {
 	client.Database(events_db_name).Drop(context.Background())
 	client.Database(profile_db_name).Drop(context.Background())
+	client.Database(checkin_db_name).Drop(context.Background())
 
 	{
 		// establishes unique id indexes to prevent duplicate documents
@@ -1367,6 +1375,116 @@ func TestGetUserTrackingInfoNone(t *testing.T) {
 
 	if !reflect.DeepEqual(recieved_tracked_events, expected_user_tracking) {
 		t.Fatalf("Wrong result received. Expected %v, got %v", expected_user_tracking, recieved_tracked_events)
+	}
+}
+
+func TestTrackUserNormal(t *testing.T) {
+	CreateEvents()
+	defer ClearEvents()
+
+	checkin_info := checkin_models.UserCheckin{
+		ID:              "localadmin",
+		Override:        false,
+		HasCheckedIn:    true,
+		HasPickedUpSwag: false,
+		RsvpData:        map[string]interface{}{},
+	}
+
+	client.Database(checkin_db_name).Collection("checkins").InsertOne(context.Background(), checkin_info)
+	defer client.Database(checkin_db_name).Collection("checkins").DeleteMany(context.Background(), bson.D{})
+
+	tracking_info := event_models.TrackingInfo{
+		EventID: "testeventid12345",
+		UserID:  "localadmin",
+	}
+
+	res_tracking := event_models.TrackingStatus{}
+	response, err := staff_client.New().Post("/event/track/").BodyJSON(tracking_info).ReceiveSuccess(&res_tracking)
+
+	if err != nil {
+		t.Fatal("Unable to make request")
+		return
+	}
+	if response.StatusCode != http.StatusOK {
+		t.Fatalf("Request returned HTTP error %d", response.StatusCode)
+		return
+	}
+
+	expected_trackers := event_models.TrackingStatus{
+		EventTracker: event_models.EventTracker{
+			EventID: "testeventid12345",
+			Users:   []string{"localadmin"},
+		},
+		UserTracker: event_models.UserTracker{
+			UserID: "localadmin",
+			Events: []string{"testeventid67890", "testeventid12345"},
+		},
+	}
+
+	if !reflect.DeepEqual(res_tracking, expected_trackers) {
+		t.Fatalf("Wrong result received. Expected %v, got %v", expected_trackers, res_tracking)
+	}
+
+	expected_event_tracker := event_models.EventTracker{
+		EventID: "testeventid12345",
+		Users:   []string{"localadmin"},
+	}
+
+	var actual_event_tracker event_models.EventTracker
+
+	res := client.Database(events_db_name).Collection("eventtrackers").FindOne(context.Background(), bson.M{"eventid": "testeventid12345"})
+
+	err = res.Decode(&actual_event_tracker)
+
+	if err != nil {
+		t.Fatalf("Had trouble finding event tracker in database: %v", err)
+		return
+	}
+
+	if !reflect.DeepEqual(actual_event_tracker, expected_event_tracker) {
+		t.Fatalf("Wrong event tracker received from database. Expected %v, got %v", expected_event_tracker, actual_event_tracker)
+	}
+
+	expected_user_tracker := event_models.UserTracker{
+		UserID: "localadmin",
+		Events: []string{"testeventid67890", "testeventid12345"},
+	}
+
+	var actual_user_tracker event_models.UserTracker
+
+	res = client.Database(events_db_name).Collection("usertrackers").FindOne(context.Background(), bson.M{"userid": "localadmin"})
+
+	err = res.Decode(&actual_user_tracker)
+
+	if err != nil {
+		t.Fatalf("Had trouble finding user tracker in database: %v", err)
+		return
+	}
+
+	if !reflect.DeepEqual(actual_user_tracker, expected_user_tracker) {
+		t.Fatalf("Wrong user tracker received from database. Expected %v, got %v", expected_user_tracker, actual_user_tracker)
+	}
+}
+
+func TestTrackUserNotCheckedin(t *testing.T) {
+	CreateEvents()
+	defer ClearEvents()
+
+	tracking_info := event_models.TrackingInfo{
+		EventID: "testeventid12345",
+		UserID:  "localadmin",
+	}
+
+	res_tracking := event_models.TrackingStatus{}
+	response, err := staff_client.New().Post("/event/track/").BodyJSON(tracking_info).ReceiveSuccess(&res_tracking)
+
+	if err != nil {
+		t.Fatal("Unable to make request")
+		return
+	}
+	if response.StatusCode != http.StatusUnprocessableEntity {
+		t.Fatalf("Request returned HTTP error %d", response.StatusCode)
+		return
 	}
 }
 

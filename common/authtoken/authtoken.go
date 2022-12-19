4d223794bff5bfd8767cd386393edad6ffadb7fa
Jareth Gomes <jarethgomes@gmail.com>
2022-12-19 02:17:35 -0600

    Upgraded jwt library to v4

--- common/authtoken/authtoken.go
+++ common/authtoken/authtoken.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"time"
 
-	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/golang-jwt/jwt/v4"
 )
 
 type Role = string
--- common/utils/jwt.go
+++ common/utils/jwt.go
@@ -1,6 +1,6 @@
 package utils
 
-import "github.com/dgrijalva/jwt-go"
+import "github.com/golang-jwt/jwt/v4"
 
 func GenerateSignedToken(secret []byte, data jwt.Claims) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, data)

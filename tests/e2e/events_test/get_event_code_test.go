6eb3896b5c37ff952bbfeefcb04b1afa83b09dc9
Jareth Gomes <jarethgomes@gmail.com>
2022-12-31 21:00:18 -0600

    Added invalid event id event code test

--- tests/e2e/events_test/get_event_code_test.go
+++ tests/e2e/events_test/get_event_code_test.go
@@ -38,6 +38,35 @@ func TestGetEventCodeNormal(t *testing.T) {
 	}
 }
 
+func TestGetEventCodeWrongEventId(t *testing.T) {
+	CreateEvents()
+	defer ClearEvents()
+
+	id := "thisisclearlythewrongid"
+	api_err := errors.ApiError{}
+	response, err := staff_client.New().Get(fmt.Sprintf("/event/code/%s/", id)).Receive(nil, &api_err)
+
+	if err != nil {
+		t.Fatal("Unable to make request")
+		return
+	}
+	if response.StatusCode != http.StatusInternalServerError {
+		t.Fatalf("Request returned HTTP error %d", response.StatusCode)
+		return
+	}
+
+	expected_error := errors.ApiError{
+		Status:   http.StatusInternalServerError,
+		Type:     "DATABASE_ERROR",
+		Message:  "Failed to receive event code information from database",
+		RawError: "Error: NOT_FOUND",
+	}
+
+	if !reflect.DeepEqual(expected_error, api_err) {
+		t.Fatalf("Wrong error response received. Expected %v, got %v", expected_error, api_err)
+	}
+}
+
 func TestGetEventCodeForbidden(t *testing.T) {
 	CreateEvents()
 	defer ClearEvents()

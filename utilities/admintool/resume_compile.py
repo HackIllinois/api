b408fa1e4f8b4c3258ebf39084795a0ef545e4bf
Jareth Gomes <jarethgomes@gmail.com>
2023-06-15 20:31:52 -0500

    Modified resume exporting script to work only outside of internal API network

--- utilities/admintool/resume_compile.py
+++ utilities/admintool/resume_compile.py
@@ -1,21 +1,59 @@
-import numpy as np
+'''
+This script collects all registrants and exports their associated resume file. All resumes are
+collected into a directory as noted by variable `DIR` and registrant data is exported to a csv under
+the name contained within `OUT_FILE`.
+
+It is reccommended to run this in a virtual env as this does require some dependencies.
+
+First time setup:
+
+Venv setup:
+```
+python3 -m venv .venv
+source .venv/bin/activate
+```
+
+Dependencies:
+```
+pip install numpy pandas requests python-magic
+```
+
+NOTE: For python-magic, you will need to do a little bit of extra setup:
+https://github.com/ahupp/python-magic#installation
+
+To run script:
+```
+HI_AUTH=your_admin_auth_token_here python resume_compile.py
+```
+'''
+import numpy as numpy
 import pandas as pd
 import requests
 import time
 import urllib.request
 import os
+import magic
+import mimetypes
+from api import make_request
 
-REGISTRATION_URL = "http://registration.api:8004/"
-UPLOAD_URL = "http://upload.api:8008/"
-
-SLEEP_NUM_SECONDS = 2
+SLEEP_NUM_SECONDS = 0.5
 DIR = './resumes'
 OUT_FILE = './resumes.csv'
 
-df = pd.DataFrame.from_records(requests.get(REGISTRATION_URL + 'registration/attendee/list/').json()['registrations'])
+res, ok = make_request("GET", '/registration/attendee/list/')
+
+if not ok:
+    print("Failed to get registration data: /registration/attendee/list/")
+    exit(1)
+
+df = pd.DataFrame.from_records(res['registrations'])
 df['resume'] = ''
 df = df.sort_values(by=['major', 'graduationYear'])
 df = df.reset_index()
+
+mime = magic.Magic(mime=True)
+
+os.makedirs(DIR, exist_ok=True)
 
 total_rows = len(df.index)
 valid_pdf_counter = 0
@@ -24,12 +62,22 @@ for i in df.index:
     id = df.iloc[i].id
 
     # get file link from upload endopint
-    link = requests.get(UPLOAD_URL + '/upload/resume/' + str(id) + '/').json()['resume']
+    res, ok = make_request("GET", '/upload/resume/' + str(id) + '/')
+
+    if not ok:
+        print(f"Failed to get resume link: /upload/resume/{str(id)}")
+        exit(1)
+
+    link = res['resume']
 
     # download file
     try:
-        filename = f'{DIR}/resume{str(valid_pdf_counter)}.pdf'
-        urllib.request.urlretrieve(link, filename)
+        filename = f'{DIR}/resume{str(valid_pdf_counter)}'
+        _, headers = urllib.request.urlretrieve(link, filename)
+        mimes = mime.from_file(filename) # Get mime type
+        ext = mimetypes.guess_all_extensions(mimes)[0] # Guess extension
+        os.rename(filename, filename+ext) # Rename file
+        filename = filename+ext
         valid_pdf_counter += 1
     except Exception as e:
         filename = ''
